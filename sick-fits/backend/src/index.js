14271396b2b9ad74d6d66c714c9e68c1c1ba452f
Ben Mallinson <ben.mallinson@drpgroup.com>
2018-12-15 13:20:30 +0000

    Yoga / Prisma and Initial Queries

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,16 @@
-// let's go!
+require('dotenv').config({path: 'variables.env'})
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server =  createServer()
+
+// use middleware for cookies and user population
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  }
+}, details => {
+  console.log(details.port)
+})
